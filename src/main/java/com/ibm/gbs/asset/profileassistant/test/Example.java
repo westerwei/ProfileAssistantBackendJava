ba9aa7ca2bc536d56431b26baa32e692f35a42e9
Wester <wester.wei@gmail.com>
2019-06-17 02:36:35 +0800

    add author

--- src/main/java/com/ibm/gbs/asset/profileassistant/test/Example.java
+++ src/main/java/com/ibm/gbs/asset/profileassistant/test/Example.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+Created by Wester
+ */
 @RestController
 @EnableAutoConfiguration
 public class Example {
